e0b30672ea8d28428889ad8341f0bc7fe5a49857
cdnguyen125 <cdnguyen125@student.hbuhsd.edu>
2025-01-08 21:03:31 +0000

    freak this class ight

--- homework.cpp
+++ homework.cpp
@@ -8,8 +8,16 @@ void displayList(std::vector<T> const &v);
 int main()
 {
     // You can follow along with any code in class here
+    std::vector<int> wholeN = {1, 2, 3, 4, 5};
+    std::vector<double> decimalN = {3.14, 2.72, 1.23, 4.56, 5.6};
+    std::vector<std::string> names = {"Amy", "Christian", "Nathaniel", "Hoangoes", "Kaden"};
 
+    std::cout << "In the int list, the values at index 0, 2, and 4 are: " << wholeN[0] <<", " << wholeN[2] << ", and " << wholeN[4] << std::endl << std::endl;
     
+    std::cout << "The list of strings/names is: " << std::endl;
+    displayList(names);
+
+
     return 0;
 }
 
